18631f2ee110f3b8d7d0b7bcca1b68a5d386665e
viktor-lima <joaolimak10@outlook.com>
2024-11-27 20:33:42 -0300

    sidebar completed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,12 +11,16 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+
+        <img className={styles.avatar} src="https://github.com/viktor-lima.png"/>
+
         <strong>João Lima</strong>
-        <span>Web Developer</span>
+        <span>FullStack Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
